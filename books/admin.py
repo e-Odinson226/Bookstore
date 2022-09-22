cc9301c698ffe64cab135f362a685872466a5b8c
zakaria <erfan.yekezare@gmail.com>
2022-09-22 15:07:07 +0330

    add book attribiutes to admin page

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Book
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "price", "author")
+
+
+admin.site.register(Book)
