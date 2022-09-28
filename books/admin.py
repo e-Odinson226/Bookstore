0ba510f47a9fc86e1786f2acf685f1f6b0edd028
zakaria <erfan.yekezare@gmail.com>
2022-09-28 12:19:05 +0330

    add books reviews to admin panel

--- books/admin.py
+++ books/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Review
+
+
+class ReviewInLine(admin.TabularInline):
+    model = Review
 
 
 class BookAdmin(admin.ModelAdmin):
+    inlines = [
+        ReviewInLine,
+    ]
     list_display = ("title", "price", "author")
 
 
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
